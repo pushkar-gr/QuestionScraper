1770a4c7df117fd2d97c37606d03ec7536f6b93e
pushkar-gr <gr.pushkar.7@gmail.com>
2025-04-13 00:02:56 +0530

    add: geeksforgeeks

---
+++ src/geeksforgeeks/client.go
@@ -0,0 +1,34 @@
+package geeksforgeeks
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const ENDPOINT = "https://geeksforgeeks.com/api/vr/" //api endpoint
+
+// send GET request to ENDPOINT
+// input: request URL
+// output: request body or error if any
+func sendRequest(url string) ([]byte, error) {
+	//send request
+	resp, err := http.Get(ENDPOINT + url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	//return if any error in https response
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("received error status code: %d", resp.StatusCode)
+	}
+
+	//read response body
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	return respBody, nil
+}
