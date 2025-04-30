e828ef89cd1545fdf93baf31bd7f250f7ab7010b
pushkar-gr <gr.pushkar.7@gmail.com>
2025-04-30 18:54:24 +0530

    add: main function

--- main.go
+++ main.go
@@ -1,3 +1,33 @@
 package main
 
-func main()  {}
+import (
+	"fmt"
+
+	"github.com/pushkar-gr/QuestionScraper/src/geeksforgeeks"
+	"github.com/pushkar-gr/QuestionScraper/src/hackerearth"
+	"github.com/pushkar-gr/QuestionScraper/src/leetcode"
+	"github.com/pushkar-gr/QuestionScraper/src/types"
+)
+
+func main() {
+	config := new(types.Config)
+	config.Update("config/config.toml")
+
+	config.Database.Init(config)
+
+	//call UpdateDB for all platforms
+	for _, platform := range config.Platforms {
+		fmt.Printf("Updating database for platform: %s\n", platform)
+
+		switch platform.Name {
+		case "GeeksForGeeks":
+			geeksforgeeks.UpdateDB(config)
+		case "LeetCode":
+			leetcode.UpdateDB(config)
+		case "HackerEarth":
+			hackerearth.UpdateDB(config)
+		default:
+			fmt.Printf("Warning: No implementation found for platform %s\n", platform)
+		}
+	}
+}
